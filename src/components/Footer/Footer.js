300a44650d770af8cf8b7b5ebae147836d553330
guedesclaudio <cgsarmentosilva@gmail.com>
2022-08-23 17:32:36 -0300

    REFACTOR: footer's layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -33,7 +33,7 @@ export default function Footer() {
                               path: {
                                 stroke: "#FFFFFF",
                               },
-                        }}/>;
+                        }}/>
                     </Circle>
                 </Link>
             </Loading>
